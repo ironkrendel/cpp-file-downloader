2366d60d41fbc53f5e4ccc31e63d3208d049adda
ironkrendel <v.a.ryshkov@gmail.com>
2026-05-27 23:32:45 +0300

    Add CMake project configuration

---
+++ apps/cppdownloader.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(const int argc, const char * argv[])
+{
+    if (argc != 4)
+    {
+        // TODO log error message and exit program
+
+        return 1;
+    }
+
+
+
+    return 0;
+}
