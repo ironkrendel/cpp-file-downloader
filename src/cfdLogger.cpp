4250e8c445fe8c7a67afa6edfdd81c7a1e94f63f
ironkrendel <v.a.ryshkov@gmail.com>
2026-05-31 01:46:13 +0300

    Change Logger to be compliant with c++11 and to use local time

--- src/cfdLogger.cpp
+++ src/cfdLogger.cpp
@@ -1,14 +1,24 @@
 #include "cfd/Logger.h"
 
 #include <iostream>
+#include <iomanip>
 #include <sstream>
 #include <chrono>
+#include <cmath>
 
 static std::string GetTimestampPrefix()
 {
-    // TODO Convert chrono timestamp to local timezone
     std::stringstream timestamp_prefix_stream;
-    timestamp_prefix_stream << "[" << std::chrono::system_clock::now() << "] ";
+
+    std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    std::tm tm_now = *std::localtime(&now);
+
+    char time_string_buffer[64];
+    std::strftime(time_string_buffer, sizeof time_string_buffer, "%Y-%m-%d %H:%M:%S", &tm_now);
+
+    auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count() % 1000;
+
+    timestamp_prefix_stream << "[" << time_string_buffer << "." << std::setfill('0') << std::setw(3) << millis << "] ";
     return timestamp_prefix_stream.str();
 }
 
